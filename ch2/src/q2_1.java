b04b5d9618db705c5f51a307c5657efda14bbd78
ellyse12 <ilkercihandurmus@gmail.com>
2025-07-17 15:33:39 +0300

    Chapter 2- Question 2

--- ch2/src/q2_1.java
+++ ch2/src/q2_1.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class q2_1 {
+    /*(Convert mile to kilometer) Write a program that reads a mile in a double value
+from the console, converts it to kilometers, and displays the result. The formula
+for the conversion is as follows:
+ 1 mile = 1.6 kilom */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter mile as double value: ");
+        double mile = sc.nextDouble();
+
+        double kilometer = mile * 1.6;
+
+        System.out.println(mile+" mile is "+kilometer+" km.");
+
+    }
 }
